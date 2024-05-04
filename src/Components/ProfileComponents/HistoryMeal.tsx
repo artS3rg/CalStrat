a1630561da9fe233505e347e25197baa85e34837
Darya <zykovadara31@gmail.com>
2024-05-04 21:19:52 +0300

    user's products and searching products

--- src/Components/ProfileComponents/HistoryMeal.tsx
+++ src/Components/ProfileComponents/HistoryMeal.tsx
@@ -1,13 +1,93 @@
-import { Box, Button, Grid, TextField, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
-import HMTable from "./HMTable";
+import { Box, Button, ClickAwayListener, Grid, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
 import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
 import dayjs, { Dayjs } from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import React from "react";
+import React, { useEffect } from "react";
+import { RootState } from "../Redux/store";
+import { UserState } from "../Redux/user";
+import { useAppSelector } from "../Redux/hooks";
+import Card from '@mui/material/Card';
 
+interface Food{
+    id: string
+    productId: string
+    sum: string
+    kcal: string
+    proteins: string
+    fats: string
+    carbohydrates: string
+    date: string
+    userId: string
+}
 export default function HistoryMeal() {
 
-    const [date, setDate] = React.useState<Dayjs | null>(dayjs());
+    const [date, setDate] = React.useState<Dayjs | null>(dayjs())
+    const selector: UserState = useAppSelector((state: RootState) => state.user)
+    const [products, setProducts] = React.useState<Food[]>([])
+
+    const [searchProductValue, setSearchProductValue] = React.useState<string>()
+    const [searchProductsList, setSearchProductsList] = React.useState<any[]>([])
+
+    const [rowIndex, setRowIndex] = React.useState(-1);
+    const [columnIndex, setColumnIndex] = React.useState(-1);
+
+    const [stateButtonAdd, setStateButtonAdd] = React.useState<boolean>(false)
+
+    const handleTextFieldChange = (rowInd: number, colName: "productId" | "sum" | "kcal" | "proteins" | "fats" | "carbohydrates", value: string) => {
+        products[rowInd][colName] = value;
+    };
+
+    const handleExit = () => {
+        setRowIndex(-1);
+        setColumnIndex(-1);
+    }
+
+    const addProduct = (product : Food) => {
+        
+    }
+
+    useEffect (() => {
+        let d = date?.format('YYYY-MM-DD')
+        console.log(d)
+        fetch('https://localhost:7129/DB/GetProduct?userID=' + selector.Id + '&date=' + d )
+        .then(
+            response => {
+                if (!response.ok) {
+                    throw new Error('Ошибка');
+                }
+                let res = response.json()
+                console.log(res)
+                return res
+            }
+        )
+        .then(
+            json => {
+                setProducts(json)
+                console.log(products)
+            }
+        )
+      }, [date])
+
+    const searchProduct = () => {
+        console.log(searchProductValue)
+        fetch('https://localhost:7129/DB/SearchProduct?product=' + searchProductValue)
+        .then(
+            response => {
+                if (!response.ok) {
+                    throw new Error('Ошибка');
+                }
+                let res = response.json()
+                console.log(res)
+                return res
+            }
+        )
+        .then(
+            json => {
+                setSearchProductsList(json)
+                console.log(searchProductsList)
+            }
+        )}
+
 
     return (
         <Box sx={{
@@ -29,7 +109,7 @@ export default function HistoryMeal() {
                 backgroundColor: "white",
                 padding: 2,
 
-            }}>
+                }}>
 
                 <Box sx={{
                     textAlign: "center"
@@ -45,6 +125,7 @@ export default function HistoryMeal() {
                                 marginBottom: 2
                             }} />
                     </LocalizationProvider>
+
                 </Box>
 
                 <Grid container sx={{
@@ -71,10 +152,250 @@ export default function HistoryMeal() {
                     </Grid>
                 </Grid>
 
-                <HMTable></HMTable>
+                <Box>
+                    <ClickAwayListener onClickAway={() => handleExit()}>
+                        <Paper sx={{ width: '100%', overflow: 'hidden', borderRadius: 5, marginBottom: 2}}>
+                        <TableContainer
+                            component={Paper}
+                            sx={{
+                            maxHeight: 300, 
+                            borderRadius: 5,
+                            backgroundColor: '#e8e8e8'
+                            }}
+                            >
+                            <Table stickyHeader aria-label="sticky table" >
+                            <TableHead>
+                                <TableRow>
+                                <TableCell sx={{fontFamily: 'Russo One', fontSize: 16, backgroundColor: '#e8e8e8'}}>Продукт</TableCell>
+                                <TableCell sx={{fontFamily: 'Russo One', fontSize: 16, backgroundColor: '#e8e8e8'}}>Грамм</TableCell>
+                                <TableCell sx={{fontFamily: 'Russo One', fontSize: 16, backgroundColor: '#e8e8e8'}}>Ккал</TableCell>
+                                <TableCell sx={{fontFamily: 'Russo One', fontSize: 16, backgroundColor: '#e8e8e8'}}>Белки</TableCell>
+                                <TableCell sx={{fontFamily: 'Russo One', fontSize: 16, backgroundColor: '#e8e8e8'}}>Жиры</TableCell>
+                                <TableCell sx={{fontFamily: 'Russo One', fontSize: 16, backgroundColor: '#e8e8e8'}}>Углеводы</TableCell>
+                                <TableCell sx={{fontFamily: 'Russo One', fontSize: 16, backgroundColor: '#e8e8e8'}}>Удалить</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {products.map((row, index) => (
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                    <TableCell
+                                    onClick={() => { setRowIndex(index); setColumnIndex(0); }}
+                                    >
+                                    {
+                                        rowIndex === index && columnIndex === 0 ?
+                                        <TextField
+                                            placeholder={row.productId}
+                                            defaultValue={products[index]["productId"]}
+                                            onChange={(event) => handleTextFieldChange(index, "productId", event.target.value)}
+                                            onKeyDown={(e) => {
+                                            if (e.key === "Enter") {
+                                                handleExit();
+                                            }
+                                            }}
+                                        /> : row.productId
+                                    }
+                                    </TableCell>
+                                    <TableCell
+                                    onClick={() => { setRowIndex(index); setColumnIndex(1); }}
+                                    >
+                                    {
+                                        rowIndex === index && columnIndex === 1 ?
+                                        <TextField
+                                            placeholder={row.sum}
+                                            defaultValue={products[index]["sum"]}
+                                            onChange={(event) => handleTextFieldChange(index, "sum", event.target.value)}
+                                            onKeyDown={(e) => {
+                                            if (e.key === "Enter") {
+                                                handleExit();
+                                            }
+                                            }}
+                                        /> : row.sum
+                                    }
+                                    </TableCell>
+                                    <TableCell
+                                    onClick={() => { setRowIndex(index); setColumnIndex(2); }}
+                                    >
+                                    {
+                                        rowIndex === index && columnIndex === 2 ?
+                                        <TextField
+                                            placeholder={row.kcal}
+                                            defaultValue={products[index]["kcal"]}
+                                            onChange={(event) => handleTextFieldChange(index, "kcal", event.target.value)}
+                                            onKeyDown={(e) => {
+                                            if (e.key === "Enter") {
+                                                handleExit();
+                                            }
+                                            }}
+                                        /> : row.kcal
+                                    }
+                                    </TableCell>
+                                    <TableCell
+                                    onClick={() => { setRowIndex(index); setColumnIndex(3); }}
+                                    >
+                                    {
+                                        rowIndex === index && columnIndex === 3 ?
+                                        <TextField
+                                            placeholder={row.proteins}
+                                            defaultValue={products[index]["proteins"]}
+                                            onChange={(event) => handleTextFieldChange(index, "proteins", event.target.value)}
+                                            onKeyDown={(e) => {
+                                            if (e.key === "Enter") {
+                                                handleExit();
+                                            }
+                                            }}
+                                        /> : row.proteins
+                                    }
+                                    </TableCell>
+                                    <TableCell
+                                    onClick={() => { setRowIndex(index); setColumnIndex(4); }}
+                                    >
+                                    {
+                                        rowIndex === index && columnIndex === 4 ?
+                                        <TextField
+                                            placeholder={row.fats}
+                                            defaultValue={products[index]["fats"]}
+                                            onChange={(event) => handleTextFieldChange(index, "fats", event.target.value)}
+                                            onKeyDown={(e) => {
+                                            if (e.key === "Enter") {
+                                                handleExit();
+                                            }
+                                            }}
+                                        /> : row.fats
+                                    }
+                                    </TableCell>
+                                    <TableCell
+                                    onClick={() => { setRowIndex(index); setColumnIndex(5); }}
+                                    >
+                                    {
+                                        rowIndex === index && columnIndex === 5 ?
+                                        <TextField
+                                            placeholder={row.carbohydrates}
+                                            defaultValue={products[index]["carbohydrates"]}
+                                            onChange={(event) => handleTextFieldChange(index, "carbohydrates", event.target.value)}
+                                            onKeyDown={(e) => {
+                                            if (e.key === "Enter") {
+                                                handleExit();
+                                            }
+                                            }}
+                                        /> : row.carbohydrates
+                                    }
+                                    </TableCell>
+                                    
+                                    <TableCell>
+                                    <Button variant="contained" sx = {{
+                                        backgroundColor: '#A9A9A9', 
+                                        fontFamily: 'Russo One',
+                                        fontSize: 12,
+                                        "&:hover": { backgroundColor: "#902B2B", },
+                                    }} 
+                                        onClick={() => { products.splice(index, 1); setProducts([...products]); }}>Удалить</Button>
+                                    </TableCell>
+
+                                </TableRow>
+                                ))}
+                            </TableBody>
+                            </Table>
+                        </TableContainer>
+                        </Paper>
+                    </ClickAwayListener>
+                   
+                </Box>
+
+                <Box>
+                    <Button variant="contained" 
+                        sx={{
+                        backgroundColor: '#902B2B', 
+                        fontFamily: 'Russo One',
+                        fontSize: 16,
+                        "&:hover": { backgroundColor: "#902B2B", },
+                        marginTop: 1,
+                        borderRadius: 2,
+                        }}
+                        onClick={() => setStateButtonAdd(true)}
+                        >
+                        Добавить прием пищи
+                    </Button>
+
+                    <Button variant="contained" sx={{
+                        backgroundColor: '#902B2B', 
+                        fontFamily: 'Russo One',
+                        fontSize: 16,
+                        "&:hover": { backgroundColor: "#902B2B", },
+                        marginTop: 1,
+                        borderRadius: 2,
+                        float: "right"
+                        }}
+                        >
+                        Сохранить
+                    </Button>
+
+                    <Grid>
+                        {
+                        stateButtonAdd === true ?
+                        <Grid>
+                            <Grid sx={{
+                                marginTop: 2
+                                }}>
+                                <TextField
+                                    label="Название продукта"
+                                    size="small"
+                                    sx={{ marginRight: 2 , width: 300}}
+                                    onChange={(e) => {setSearchProductValue(e.target.value)}} />
+                                <Button variant="contained" sx={{
+                                    backgroundColor: '#A9A9A9', 
+                                    fontFamily: 'Russo One',
+                                    fontSize: 14,
+                                    "&:hover": { backgroundColor: "#902B2B", },
+                                    borderRadius: 2,
+                                    }}
+                                    onClick={searchProduct}
+                                    >
+                                    Поиск
+                                </Button>
+                                <Box sx={{
+
+                                }}>
+                                {
+                                    searchProductsList.map((product) => {
+                                        return (
+                                            <Card variant="outlined" sx={{
+                                                marginY: 2,
+                                                padding: 2
+                                            }}>
+                                                <Stack direction="row" justifyContent="space-between">
+                                                    <Typography >{product.name}</Typography>
+                                                    <Button variant="contained" sx={{
+                                                        backgroundColor: '#A9A9A9', 
+                                                        fontFamily: 'Russo One',
+                                                        fontSize: 12,
+                                                        "&:hover": { backgroundColor: "#902B2B", },
+                                                        borderRadius: 2,
+                                                        float: "right"
+                                                        }}
+                                                        onClick={() => addProduct(product)}
+                                                        >
+                                                        Добавить
+                                                    </Button>
+                                                </Stack>
+                                                
+                                            </Card>
+                                        )
+                                    })
+                                }
+                                </Box>
+                            </Grid>
+                            
+                            
+                        </Grid>
+                        : <p></p>
+                        }
+                    </Grid>
+                </Box>
             </Box>
 
 
         </Box>
     )
 }
+
+
